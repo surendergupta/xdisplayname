39268a92d3ec82efade785f89bc36c9370bd1644
surendergupta <gupta.surender.1990@gmail.com>
2024-09-04 11:27:42 +0530

    local commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(firstName.trim() && lastName.trim() && firstName.length > 0 && lastName.length > 0) {
+      document.getElementById('display').innerHTML = `Full Name: ${firstName} ${lastName}`;
+    }
+    else
+    {
+      document.getElementById('display').innerHTML = ``;
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Full Name Display</h1>
+      <form className='form' onSubmit={handleSubmit}>
+        <div className=''>
+          <label>
+            FirstName: 
+            <input 
+              className='' 
+              type="text" 
+              onChange={(e) => setFirstName(e.target.value)} 
+              value={firstName} 
+              placeholder='Enter first name' 
+              required={true} 
+            />
+          </label>
+        </div>
+        <div className=''>
+          <label>
+            LastName: 
+            <input 
+              className=''  
+              type="text" 
+              onChange={(e) => setLastName(e.target.value)} 
+              value={lastName} 
+              placeholder='Enter last name' 
+              required={true} 
+            />
+          </label>
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
+
+      <div id='display'></div>
     </div>
   );
 }
